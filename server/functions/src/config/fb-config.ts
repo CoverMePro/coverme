e149431815d1f7387d8fb5ff6b9c87ebf9edf697
Matthew Douglas <matt.w.doug@gmail.com>
2021-12-07 20:18:00 -0500

    Set up firebase function in server folder

---
+++ server/functions/src/config/fb-config.ts
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyAWKeAFzmKcmcRz1si1NmmoSQCIxkO_90M',
+  authDomain: 'coverme-47dc7.firebaseapp.com',
+  projectId: 'coverme-47dc7',
+  storageBucket: 'coverme-47dc7.appspot.com',
+  messagingSenderId: '15415320328',
+  appId: '1:15415320328:web:e39295f287accb8285d6bd',
+  measurementId: '${config.measurementId}',
+};
---
+++ server/functions/src/index.ts
@@ -0,0 +1,17 @@
+import * as functions from 'firebase-functions';
+import express from 'express';
+
+// setup express
+const app = express();
+
+// routes
+
+exports.api = functions.https.onRequest(app);
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
