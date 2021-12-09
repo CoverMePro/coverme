dda3fede375ba4b05d1b659599b2139767568483
douglas1701 <matt.w.doug@gmail.com>
2021-12-09 16:34:48 -0500

    Material UI Implementation and a Mock Login Page

--- client-web/src/index.tsx
+++ client-web/src/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import Login from 'pages/Login';
+import Dashboard from 'pages/Dashboard';
+
 import './index.css';
-import App from './App';
+
+import { ThemeProvider } from '@mui/material';
+import { theme } from './theme';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider theme={theme}>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/login" element={<Login />} />
+					<Route path="/dasbhoard" element={<Dashboard />}></Route>
+					<Route path="/" element={<Navigate replace to="/login" />} />
+				</Routes>
+			</BrowserRouter>
+		</ThemeProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
--- client-web/src/theme.ts
+++ client-web/src/theme.ts
@@ -1,37 +1,20 @@
-import { ThemeOptions } from '@material-ui/core/styles/createMuiTheme';
+import { createTheme } from '@mui/material/styles';
 
-export const themeOptions: ThemeOptions = {
-  palette: {
-    type: 'light',
-    primary: {
-      main: '#5C2DB5',
-      contrastText: '#ffffff',
-    },
-    secondary: {
-      main: '#819595',
-      contrastText: 'rgba(255,255,255,0.87)',
-    },
-    background: {
-      default: '#F6F6F6',
-      paper: '#fffffa',
-    },
-    text: {
-      primary: '#2e2727',
-      secondary: '#2e2727',
-      disabled: 'rgba(46,39,39,0.48)',
-      hint: '#2e2727',
-    },
-    error: {
-      main: '#F8333C',
-    },
-    warning: {
-      main: '#fcab10',
-    },
-    info: {
-      main: '#72ccfe',
-    },
-    success: {
-      main: '#00BE82',
-    },
-  },
-};
+export const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#006d77',
+			contrastText: '#ffffff'
+		},
+		secondary: {
+			main: '#83c5be'
+		},
+		background: {
+			default: '#F6F6F6',
+			paper: '#FFFFFA'
+		},
+		text: {
+			primary: '#2E2727'
+		}
+	}
+});
