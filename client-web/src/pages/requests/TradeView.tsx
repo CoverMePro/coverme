dbcec805187c1cd88519debee8697cb98331c6f6
Matthew Douglas <matt.w.doug@gmail.com>
2022-02-24 08:42:42 -0500

    added tabs to trades view

--- client-web/src/pages/requests/TradeView.tsx
+++ client-web/src/pages/requests/TradeView.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Box, Tabs, Tab, Typography } from '@mui/material';
 
 const TradeView: React.FC = () => {
-    return <div>TRADE</div>;
+    const [tabValue, setTabValue] = useState<number>(0);
+
+    const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+        setTabValue(newValue);
+    };
+
+    return (
+        <Box>
+            <Typography variant="h2">Trade Requests</Typography>
+            <Tabs value={tabValue} onChange={handleTabChange} centered variant="fullWidth">
+                <Tab label="Pending" />
+                <Tab label="Confirmed" />
+                <Tab label="Denied" />
+            </Tabs>
+        </Box>
+    );
 };
 
 export default TradeView;
